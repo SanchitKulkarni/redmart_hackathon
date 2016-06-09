46d2a40afb665abd9d805a2f8e13721ecc114203
Sanchit G Kulkarni <sanchitkulkarni3@gmail.com>
2016-06-09 14:38:12 +0530

    FIrst COmmit

---
+++ app/Global.java
@@ -0,0 +1,25 @@
+import org.mongodb.morphia.Morphia;
+import com.mongodb.MongoClient;
+import controllers.MorphiaObject;
+import play.GlobalSettings;
+import play.Logger;
+
+public class Global extends GlobalSettings{
+
+    @Override
+    public void onStart(play.Application argo) {
+        super.beforeStart(argo);
+        Logger.debug("** on start **");
+        try {
+            MorphiaObject.mongo = new MongoClient("127.0.0.1", 27017);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        MorphiaObject.morphia = new Morphia();
+        MorphiaObject.datastore = MorphiaObject.morphia.createDatastore(MorphiaObject.mongo, "redmart_todo");
+        MorphiaObject.datastore.ensureIndexes();
+        MorphiaObject.datastore.ensureCaps();
+
+        Logger.debug("** Morphia datastore: " + MorphiaObject.datastore.getDB());
+    }
+}
